1cf6c17e97b11355a9f364b0cc1edbac2c605696
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2021-07-06 20:34:55 +0900

    ミックスインの実装

--- chapter5/src/index.ts
+++ chapter5/src/index.ts
@@ -160,3 +160,36 @@ let state: State = {'hoge': '1', 'fuga': '2', 'piyo': '3'}
 let database = StringDatabase.from(state)
 database.set('hogehoge', '4')
 console.log(database)
+
+//ミックスイン
+type ClassConstructor<T> = new (...args: any[]) => T
+function withEZDebug<C extends ClassConstructor<{getDebugValue(): object}>>(Class: C) {
+    return class extends Class {
+        constructor(...args: any[]) {
+            super(...args)
+        }
+        debug() {
+            let name = this.constructor.name
+            let value = this.getDebugValue()
+            return name + '(' + JSON.stringify(value) + ')'
+        }
+    }
+}
+
+class HardToDebugUser {
+    constructor(
+        private id: number,
+        private firstName: string,
+        private lastName: string
+    ){}
+    getDebugValue() {
+        return {
+            id: this.id,
+            name: this.firstName + this.lastName
+        }
+    }
+}
+
+let User = withEZDebug(HardToDebugUser)
+let user = new User(3, 'ootsuka', 'tomoaki')
+console.log(user.debug())
